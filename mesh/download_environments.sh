fade31727434b76dfe2fc4aaf0f4b52f0852a4de
ray0727 <rayhuang100@gmail.com>
2022-03-07 22:16:42 +0800

    update URL

--- mesh/download_environments.sh
+++ mesh/download_environments.sh
@@ -6,14 +6,13 @@ echo ""
 echo "Downloading simulation environments..."
 echo ""
 
-ggID='1E_5hmG03VTHGOIeyAPJ8lGw-sKW4e5Wh'
+ggID='19Z9GJjG-H34WpE_lON0qYFkzD-fb7x_j'
 ggURL='https://drive.google.com/uc?export=download'
 
 filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"
 getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"
 
 curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "${filename}"
-
 echo ""
 echo "Unzipping files..."
 echo ""
@@ -23,5 +22,5 @@ unzip "${filename}"
 rm "${filename}"
 
 echo ""
-echo "Done, simulation environments are kept in 'src/nctu_simulation_env/mesh'."
+echo "Done, simulation environments are kept in 'src/real_to_sim_env/mesh'."
 echo ""
